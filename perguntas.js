0ea7ace0670d315fb1fba65a5dccef0ac8b1d47f
anandadealmeida <142987112+anandadealmeida@users.noreply.github.com>
2024-10-21 07:15:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Disney',
+    'Qual parque é o melhor ?',
+    'O melhor parque é Magic Kingdom é quase uma unanimidade como o favorito'
+)
+
+criaCartao(
+    'Disney',
+    'Quais são os parque em Orlando?',
+    'Walt Disney World, Universal Orlando e Sea World.'
+)
+
+criaCartao(
+    'Disney',
+    'Quanto custa a entrada do parque da Disney em Orlando?',
+    ' a partir de US$ 89,00 por dia para visita de três parques e a partir de US$ 99,00 por dia para visita de 4 parques'
+)
+
+criaCartao(
+    'Disney',
+    'Qual é a Disney original?',
+    'A Disney original é a Disneyland, localizada em Anaheim, na Califórnia'
+)
